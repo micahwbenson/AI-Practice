df4d277e297f70a821e55970d06d1906b379be85
Micah Benson <38742670+micahwbenson@users.noreply.github.com>
2021-12-10 23:20:34 +0000

    10 Dec - Implemented Grid

--- AI-Practice/Assets/Scripts/BFS/GridBFS.cs
+++ AI-Practice/Assets/Scripts/BFS/GridBFS.cs
@@ -19,6 +19,8 @@ public class GridBFS : MonoBehaviour
 
         gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
         gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+
+        CreateGrid();
     }
 
     // Update is called once per frame
@@ -32,12 +34,33 @@ public class GridBFS : MonoBehaviour
         grid = new NodeBFS[gridSizeX, gridSizeY];
 
         //Ok, if I remember this correctly, we need a place to start dropping in Nodes - the bottom left corner - ok, need to refresh a bit on the vector math here . . . but stuff is starting to stick!!!
-        //Vector2 gridBottomLeft = transform.position - Vector2.right * gridWorldSize.x / 2 - Vector2.up * nodeDiameter + nodeRadius;
+        Vector3 gridBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.up * gridWorldSize.y / 2;
+
+        //Ok now you need to loop through your gridSizeX and GridSizeY and instantiate a node at each point . . . even though my node class doesn't currently have a constructor in it
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                //Ok so we need a vector for each location so it's a calculation based off the x and y variables and using the gridBottomLeft as a starting point
+                Vector2 worldPoint = gridBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.up * (y * nodeDiameter + nodeRadius);
+                //Ok, now we have a point . . . so using that we want to use the constructor created in the node class to build a node at this location
+                grid[x, y] = new NodeBFS(x, y, worldPoint);
+            }
+        }
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
         Gizmos.DrawWireCube(transform.position, gridWorldSize);
+
+        if (grid != null)
+        {
+            Gizmos.color = Color.white;
+            foreach (NodeBFS n in grid)
+            {
+                Gizmos.DrawWireCube(n.worldPosition, Vector3.one * (nodeDiameter - 0.05f));
+            }
+        }
     }
 }
--- AI-Practice/Assets/Scripts/BFS/NodeBFS.cs
+++ AI-Practice/Assets/Scripts/BFS/NodeBFS.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 
 public class NodeBFS
 {
-    
+    public Vector3 worldPosition;
+    public int gridX, gridY;
+
+    //This is used for a* later on, but it's not something I need to worry about yet for BFS
+    //int gCost, hCost;
+
+
+    public NodeBFS (int _gridX, int _gridY, Vector3 _worldPosition)
+    {
+        gridX = _gridX;
+        gridY = _gridY;
+        worldPosition = _worldPosition;
+    }
 }
